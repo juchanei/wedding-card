f17fab4223ec8fa12a9bac2adbe7136b9b4035cc
이주찬[Plasma] <juchan.lee@navercorp.com>
2023-12-10 17:34:29 +0900

    디자인 없는 html 구조 및 컨텐츠 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,111 +3,146 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <article>
+        <div>
+          <h1 className="text-6xl font-bold text-left">
+            24.02.24
+            <br />
+            SAT.PM 14:00
+          </h1>
+          <Image
+            src="/vercel.svg"
+            alt="Title Wedding Image"
+            className="dark:invert"
+            width={100}
+            height={24}
+            priority
+          />
+          <h1 className="text-6xl font-bold text-right">
+            <span>이수인</span>
+            {` - `}
+            <span>이주찬</span>
+          </h1>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        {/* 초대 문구 섹션 */}
+        <section className="flex flex-col justify-between">
+          <h2>초대합니다</h2>
+          <p>
+            7년 전 연인으로 만난 저희가
+            <br />
+            가족이 되고자 결혼합니다.
+            <br />
+            즐겁게 잘 살겠습니다.
+            <br />
+            귀한 걸음 하시어 축하해주세요.
           </p>
-        </a>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        {/* 전화번호 섹션 */}
+        <section>
+          <p>
+            <div>
+              이철수, 인순애의 차남 <span>이주찬</span>
+              <CallLink phoneNumber='010-4364-3208' />
+            </div>
+            <div>
+              이현일, 차미경의 장녀 <span>이수인</span>
+              <CallLink phoneNumber='010-4077-9142' />
+            </div>
           </p>
-        </a>
+          <h3>신랑 측 혼주</h3>
+          <p>
+            <div>
+              <span>부</span> 이철수
+              <CallLink phoneNumber='010-3182-2689' />
+            </div>
+            <div>
+              <span>모</span> 인순애
+              <CallLink phoneNumber='010-4155-3208' />
+            </div>
+          </p>
+          <h3>신부 측 혼주</h3>
+          <p>
+            <div>
+              <span>부</span> 이현일
+              <CallLink phoneNumber='010-4596-0717' />
+            </div>
+            <div>
+              <span>모</span> 차미경
+              <CallLink phoneNumber='010-4596-3885' />
+            </div>
+          </p>
+        </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        {/* 사진첩 섹션 */}
+        <section>
+          <h2>사진첩</h2>
+          <div>
+            {/* 캐로셀 사진첩 구현 */}
+          </div>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        {/* 날짜 섹션 */}
+        <section className='flex flex-row items-center'>
+          <div>
+            2024.
+            <br />
+            02.24
+            <br />
+            토요일
+          </div>
+          <div>-</div>
+          <div>
+            PM
+            <br />
+            14:00
+          </div>
+        </section>
+
+        {/* 장소 섹션 */}
+        <section>
+          <h2>오시는 길</h2>
+          <p>
+            {/* 지도 구현 */}
           </p>
-        </a>
-      </div>
+          <p>
+            서울시 강남구 영동대로 506
+            <br />
+            (삼성동 168-3, 더베일리하우스)
+            <br />
+            TEL) 02-539-2956
+          </p>
+          <hr />
+          <h3>주차</h3>
+          <p>블라블라~</p>
+          <h3>대중교통</h3>
+          <p>블라블라~</p>
+        </section>
+
+        {/* 마음 전하실 곳 섹션 */}
+        <section>
+          <h2>마음 전하실 곳</h2>
+          <h3>신랑 측 계좌번호</h3>
+          <p>블라블라~</p>
+          <h3>신부 측 계좌번호</h3>
+          <p>블라블라~</p>
+        </section>
+
+        {/* 공유하기 섹션 */}
+        <section>
+          <p>카카오 톡으로 공유하기</p>
+          <p>청첩장 링크 복사하기</p>
+        </section>
+      </article>
     </main>
   )
+}
+
+function CallLink({ phoneNumber }: { phoneNumber: string }) {
+  return <a href={`tel:${phoneNumber}`}><CallIcon /></a>
+}
+
+function CallIcon() {
+  return <>Call</>
 }
