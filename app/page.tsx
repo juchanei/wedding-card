5908b9e1f56d2c11082103e0103fd50b6d1fc8a0
LEE Juchan <juchanei.lee@gmail.com>
2023-12-19 00:11:24 +0900

    날짜 섹션 텍스트 스타일 업데이트

--- app/page.tsx
+++ app/page.tsx
@@ -77,16 +77,16 @@ export default function Home() {
 
       {/* 날짜 섹션 */}
       <Section>
-        <div className="flex flex-row items-center justify-between bg-white w-full px-5 py-24">
-          <div>
+        <div className="flex flex-row items-center justify-center bg-white w-full px-5 py-24 font-blod text-3xl">
+          <div className='text-center'>
             2024.
             <br />
             02.24
             <br />
             토요일
           </div>
-          <div>-</div>
-          <div>
+          <div className='mx-10'>-</div>
+          <div className='text-center'>
             PM
             <br />
             14:00
